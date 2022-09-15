b0b799e63dbb877a2ec52637bcfcde9e9e58b567
faazizpro <fahadafs11@gmail.com>
2022-09-15 10:56:59 +0600

    Fixed countdown and removed from the display afetr 0

--- script.js
+++ script.js
@@ -105,16 +105,17 @@ const start = () => {
 
   let count = 3;
   countdownOverlay.style.display = "flex";
+  // countdownOverlay.style.display = "none";
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
-      // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
       display.classList.remove("inactive");
+      countdownOverlay.style.display = "none";
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
